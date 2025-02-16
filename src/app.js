f0471940975d0d74afdf7520a4ddadaf555ddad3
gabizinyk <gabi.zinyk@gmail.com>
2025-02-15 23:03:51 -0300

    Added addProudct class v1

--- src/app.js
+++ src/app.js
@@ -3,19 +3,43 @@ const classProductManager = require("./dao/ProductManager.js");
 const productManager = new classProductManager("./src/data/products.json");
 
 const app = express();
+app.use(express.json());
 const PORT = 8000;
 
 app.get("/api/products", async (req, res) => {
   let products = await productManager.getProducts();
   res.status(200).send(products);
-})
+});
 
 app.get("/api/products/:pid", async (req, res) => {
-  let {pid} = req.params
-  let product = await productManager.getProductsById(pid)
-  res.send(product)
-})
+  let { pid } = req.params;
+  let product = await productManager.getProductsById(pid);
+
+  if (!product) {
+    res.status(404).send("No existe el producto");
+    return;
+  }
+
+  res.status(200).send(product);
+});
+
+app.post("/api/products", async (req, res) => {
+  let { title, description, code, price, status, stock, category, thumbnails } =
+    req.body;
+  await productManager.addProduct(
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnails
+  );
+ 
+  res.status(200).send("Producto agregado exitosamente");
+});
 
 app.listen(PORT, () => {
   console.log(`Ejecutando app en el puerto ${PORT}`);
-})
+});
--- src/dao/ProductManager.js
+++ src/dao/ProductManager.js
@@ -1,4 +1,5 @@
 const fs = require("fs/promises");
+const { v4: uuidv4 } = require('uuid');
 
 class ProductManager {
   constructor(path) {
@@ -7,28 +8,56 @@ class ProductManager {
 
   async getProducts() {
     try {
-      const products = await fs.readFile(this.path, "utf-8")
-      return products
+      const products = await fs.readFile(this.path, "utf-8");
+      return products;
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
   async getProductsById(pid) {
     if (!pid) {
       console.log("No se ha ingresado un ID");
-      return
+      return;
     }
 
-    let products = await this.getProducts()
-    let product = JSON.parse(products, null).find(p => p.id === Number(pid))
+    let products = await this.getProducts();
+    let product = JSON.parse(products, null).find((p) => p.id === Number(pid));
 
-    return (!product) ? console.log('No existe el producto') : product   
+    return !product ? console.log("No existe el producto") : product;
   }
 
-  /* async addProducts(){
-        const products = this.getProducts()
-    } */
+  async addProduct(
+    title,
+    description,
+    code,
+    price,
+    status,
+    stock,
+    category,
+    thumbnails
+  ) {
+    // Validaciones
+    let products = await this.getProducts();
+    let data = JSON.parse(products);
+
+    let newProduct = {
+      id: uuidv4(),
+      title: title,
+      description: description,
+      code: code,
+      price: price,
+      status: status,
+      stock: stock,
+      category: category,
+      thumbnails: thumbnails,
+    };
+
+    data.push(newProduct);
+    let productAdded = JSON.stringify(data);
+
+    await fs.writeFile(this.path, productAdded, "utf-8");
+  }
 }
 
 module.exports = ProductManager;
