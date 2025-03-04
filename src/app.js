de53349b237bc83cfa26a783a59dab69503eec12
gabizinyk <gabi.zinyk@gmail.com>
2025-03-04 20:41:18 -0300

    Added handlebars

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const handlebars = require('express-handlebars');
 const productRouter = require("./routes/productRoutes.js");
 const cartRouter = require("./routes/cartRoutes.js");
 const uploadsRouter = require("./routes/uploadRoutes.js");
@@ -8,13 +9,19 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
+app.set('views', './src/views')
+app.use(express.static('/views')) 
+
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 app.use("/api/uploads", uploadsRouter);
 
 app.get("/", (req, res) => {
+  res.render('index');
   res.setHeader("Content-Type", "text/html");
-  res.status(200).json("OK");
+  res.status(200)
 });
 
 app.listen(PORT, () => {
